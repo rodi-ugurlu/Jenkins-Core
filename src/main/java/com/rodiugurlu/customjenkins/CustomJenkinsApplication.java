9fc9f5be6b26d960e792b48a6cea7652301861f6
rodi-ugurlu <rodiiugurlu@gmail.com>
2025-09-30 22:26:02 +0300

    first commit

---
+++ src/main/java/com/rodiugurlu/customjenkins/CustomJenkinsApplication.java
@@ -0,0 +1,13 @@
+package com.rodiugurlu.customjenkins;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomJenkinsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CustomJenkinsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/rodiugurlu/customjenkins/CustomJenkinsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rodiugurlu.customjenkins;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomJenkinsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
